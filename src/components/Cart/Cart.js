866d9a43de8a142f4a5f55ca30a1c1db0da6329a
sowmik-sec <sowmik.sec@gmail.com>
2023-02-15 10:52:59 +0600

    some cart issue fixed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,24 @@
 import React from "react";
+import Product from "../Product/Product";
 import "./Cart.css";
 
 const Cart = ({ cart }) => {
   let total = 0,
-    shipping = 0;
-  cart.forEach((cart) => ((total += cart.price), (shipping += cart.shipping)));
+    shipping = 0,
+    quantity = 0;
+  cart.forEach(
+    (cart) => (
+      (total += cart.price * cart.quantity),
+      (shipping += cart.shipping),
+      (quantity += cart.quantity)
+    )
+  );
   const tax = total * 0.1;
   const grandTotal = total + shipping + tax;
   return (
     <div className="cart">
       <h4>Order Summary</h4>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {quantity}</p>
       <p>Total price: ${total}</p>
       <p>Total Shipping: ${shipping}</p>
       <p>Tax: ${tax.toFixed(2)}</p>
